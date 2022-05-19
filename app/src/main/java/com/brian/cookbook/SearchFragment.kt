362d16e888488d779c8f8464701ad458ffbba5b3
bryanares <bryanares12@gmail.com>
2022-05-19 13:10:16 +0300

    format

--- app/src/main/java/com/brian/cookbook/SearchFragment.kt
+++ app/src/main/java/com/brian/cookbook/SearchFragment.kt
@@ -7,14 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
+class SearchFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
